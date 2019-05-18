c0ae78db692bd13331e7d8523377975527bcd338
LauDela <laudela@gmail.com>
2019-05-18 14:32:17 +0200

    Update action-Search-album.py

--- action-Search-album.py
+++ action-Search-album.py
@@ -55,9 +55,9 @@ def action_wrapper(hermes, intentMessage, conf):
         url = "http://" + addr_ + ":" + port_ + "/jsonrpc?request=" + request
         response = requests.get(url)
         json_data = simplejson.loads(response.text)
-        album = json_data['albums']['title'][0]
-        print 'Retour:'.album
-        #hermes.publish_end_session(current_session_id, "Album trouvé " + album)
+        #album = json_data['albums']['title'][0]
+        #print 'Retour:'.json_data
+        hermes.publish_end_session(current_session_id, "Album trouvé ")
 
     try:           
         #openAddon()
