92e036af17530144a34b23ce78599312a28a5e42
LauDela <laudela@gmail.com>
2019-05-18 18:51:56 +0200

    Update action-Play.py

--- action-Play.py
+++ action-Play.py
@@ -31,7 +31,7 @@ def on_message(client, userdata, msg):
         payload = msg.payload
         print("playing some good stuff")
         playong_file_number = random.randint(1, len(sound_files))
-        with open("sounds/" + sound_files[playong_file_number], "rb") as sound_file:
+        #with open("sounds/" + sound_files[playong_file_number], "rb") as sound_file:
         with open("music/Hotel California.mp3", "rb") as sound_file:
             imagestring = sound_file.read()
             byte_array = bytearray(imagestring)
