86dee26d35e91eccfdce1015b9455177938cc724
LauDela <laudela@gmail.com>
2019-05-25 17:51:55 +0200

    Update action-Search-album.py

--- action-Search-album.py
+++ action-Search-album.py
@@ -52,9 +52,9 @@ def searchAlbum(hermes, intentMessage):
   url = "http://" +user_+":"+password_+"@"+ addr_ + ":" + port_ + "/jsonrpc?request=" + request
   print("Creation de la playlist")
   response = requests.get(url)
-  request ="{\"jsonrpc\": \"2.0\", \"method\": \"Playlist.Add\", \"params\": { \"item\": { \"albumid\":" +albumid+"}, \"playlistid\": 1 }}"
+  request ="{\"jsonrpc\": \"2.0\", \"method\": \"Playlist.Add\", \"params\": { \"item\": { \"albumid\":"+ albumid +"}, \"playlistid\": 1 }}"
   url = "http://" +user_+":"+password_+"@"+ addr_ + ":" + port_ + "/jsonrpc?request=" + request
-  print("Ajout a la playlist")
+  print("Ajout a la playlist" + url)
   response = requests.get(url)
   request ="{\"jsonrpc\": \"2.0\", \"id\": 2, \"method\": \"Player.Open\", \"params\": {\"item\": {\"playlistid\": 1}}}"
   url = "http://" +user_+":"+password_+"@"+ addr_ + ":" + port_ + "/jsonrpc?request=" + request
