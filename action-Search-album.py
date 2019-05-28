053c6a76aa55c1c2b7775bd1b9a03841bfc16dbe
LauDela <laudela@gmail.com>
2019-05-28 11:06:44 +0200

    Update action-Search-album.py

--- action-Search-album.py
+++ action-Search-album.py
@@ -52,12 +52,12 @@ def searchAlbum(hermes, intentMessage):
   url = "http://" +user_+":"+password_+"@"+ addr_ + ":" + port_ + "/jsonrpc?request=" + request
   print("Creation de la playlist " + url)
   response = requests.get(url)
-  request ="{\"jsonrpc\": \"2.0\", \"method\": \"Playlist.Add\", \"params\": {\"playlistid\": 1, \"item\": { \"albumid\":113}}}"
+  request ="{\"jsonrpc\": \"2.0\", \"id\": 0, \"method\": \"Playlist.Add\", \"params\": {\"playlistid\": 1, \"item\": { \"albumid\":113}}}"
   url = "http://" +user_+":"+password_+"@"+ addr_ + ":" + port_ + "/jsonrpc?request=" + request
   print("Ajout a la playlist " + url)
   response = requests.get(url)
   print(response)
-  request ="{\"jsonrpc\": \"2.0\", \"id\": 2, \"method\": \"Player.Open\", \"params\": {\"item\": {\"playlistid\": 1}}}"
+  request ="{\"jsonrpc\": \"2.0\", \"id\": 0, \"method\": \"Player.Open\", \"params\": {\"item\": {\"playlistid\": 1}}}"
   url = "http://" +user_+":"+password_+"@"+ addr_ + ":" + port_ + "/jsonrpc?request=" + request
   print("lecture de la playlist " + url)
   response = requests.get(url)
