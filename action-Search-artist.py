f50af762ba32394e76beae432d9d5606e2ccd8c2
LauDela <laudela@gmail.com>
2019-08-12 23:37:39 +0200

    Update action-Search-artist.py

--- action-Search-artist.py
+++ action-Search-artist.py
@@ -69,7 +69,7 @@ def action_genereliste(hermes, intentMessage,artistid,conf):
   zone = soco.SoCo('192.168.10.4')  
   zone.clear_queue()
   zone.stop()
-  
+  hermes.publish_continue_session(current_session_id,"LA liste de lecture est en préparation. Veuillez patienter...",["LauDela:Search-artist"])
   data = '{"id":"160","jsonrpc":"2.0","method":"Playlist.Clear","params":{"playlistid":1}}'
   response = requests.post(kodi_url, headers=headers, data=data)
   json_obj= response.text
@@ -85,7 +85,7 @@ def action_genereliste(hermes, intentMessage,artistid,conf):
   json_obj= response.text
   json_data = simplejson.loads(response.text)
   #hermes.publish_end_session(current_session_id, "LA liste de lecture est en préparation. Veuillez patienter...")
-  hermes.publish_continue_session(current_session_id,"LA liste de lecture est en préparation. Veuillez patienter...",["LauDela:Search-artist"])
+  
   for song in json_data['result']['items']:
     songId = song['id']
     data='{"jsonrpc": "2.0", "id": 1, "method": "AudioLibrary.GetSongDetails", "params": {"songid": '+str(songId)+', "properties": ["title", "album", "artist","file"]}}'
